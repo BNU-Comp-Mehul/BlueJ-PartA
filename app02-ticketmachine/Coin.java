50a8f256bfde3ccac5275391b28cbad82754edea
mehulpatel1 <mehulpatel_@outlook.com>
2020-10-22 23:13:53 +0100

    Coin class has been added

---
+++ app02-ticketmachine/Coin.java
@@ -0,0 +1,25 @@
+/**
+ * Enumeration class Coin - write a description of the enum class here
+ *
+ * @author Mehul Patel
+ * Modified on 22.10.2020
+ */
+public enum Coin
+{
+        P10 (10),
+        P20 (20),
+        P100 (100),
+        P200 (200);
+
+        private final int value;
+
+        private Coin(int value)
+        {
+            this.value = value;
+        }
+
+        public int getValue()
+        {
+            return value;
+        }
+}
