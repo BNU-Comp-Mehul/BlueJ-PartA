fa3e2bab1e6695f22fc202be3de14deda5756813
mehulpatel1 <mehulpatel_@outlook.com>
2020-10-22 23:16:14 +0100

    Ticket Machine has been modified

---
+++ app02-ticketmachine/TicketMachine.java
@@ -0,0 +1,166 @@
+/**
+ * TicketMachine models a ticket machine that issues
+ * flat-fare tickets.
+ * The price of a ticket is specified via the constructor.
+ * Instances will check to ensure that a user only enters
+ * sensible amounts of money, and will only print a ticket
+ * if enough money has been input.
+ * 
+ * @author Mehul Patel
+ * Modified on 22.10.2020
+ */
+public class TicketMachine
+{
+    // The amount of money entered by a customer so far.
+    private int balance;
+    // The total amount of money collected by this machine.
+    private int total;
+    
+    private Ticket aylesburyTicket;
+    
+    private Ticket amershamTicket;
+    
+    private Ticket highwycombeTicket;
+    
+    private Ticket issuedTicket;
+    
+
+    /**
+     * Create a machine that issues tickets of the given price.
+     */
+    public TicketMachine()
+    {
+        balance = 0;
+        total = 0;
+        
+        aylesburyTicket = new Ticket("Aylesbury", 220);
+        
+        amershamTicket = new Ticket("Amersham", 300);
+        
+        highwycombeTicket = new Ticket("High Wycombe", 330);
+        
+        issuedTicket = null;
+    }
+
+    /**
+     * Return The amount of money already inserted for the
+     * next ticket.
+     */
+    public int getBalance()
+    {
+        return balance;
+    }
+
+    /**
+     * Receive an amount of money from a customer.
+     * Check that the amount is sensible.
+     */
+    public void insertMoney(int amount)
+    {
+        if(amount > 0) 
+        {
+            balance = balance + amount;
+        }
+        else 
+        {
+            System.out.println("Use a positive amount rather than: " +
+                               amount);
+        }
+    }
+
+    /**
+     * Print a ticket if enough money has been inserted, and
+     * reduce the current balance by the ticket price. Print
+     * an error message if more money is required.
+     */
+    public void printTicket()
+    {
+        int price = issuedTicket.getPrice();
+        
+        if(balance >= price)
+        {
+            // Simulate the printing of a ticket.
+            System.out.println("##################");
+            System.out.println("# The BlueJ Line");
+            issuedTicket.print();
+            System.out.println("##################");
+            System.out.println();
+
+            // Update the total collected with the price.
+            total = total + price;
+            // Reduce the balance by the price.
+            balance = balance - price;
+        }
+        else 
+        {
+            System.out.println("You must insert at least: " +
+                               (price - balance) + " more pence.");
+                    
+        }
+    }
+
+    /**
+     * Return the money in the balance.
+     * The balance is cleared.
+     */
+    public int refundBalance()
+    {
+        int amountToRefund;
+        amountToRefund = balance;
+        balance = 0;
+        return amountToRefund;
+    }
+    
+    public void printAllTickets()
+    {
+        System.out.println("The following three destinations are available");
+        aylesburyTicket.print();
+        amershamTicket.print();
+        highwycombeTicket.print();
+        System.out.println("Please select your ticket destination > ");
+    }
+    
+    public void selectTicket(String whereTo)
+    {
+        if(whereTo.startsWith("Ayl"))
+        {
+            issuedTicket = aylesburyTicket;
+        }
+        else if(whereTo.startsWith("Ame"))
+        {
+            issuedTicket = amershamTicket;
+        }
+        else if(whereTo.startsWith("highwycombe"))
+        {
+            issuedTicket = highwycombeTicket;
+        }
+        else 
+        {
+            issuedTicket = null;
+            System.out.println("Invalid Destination");
+        }
+            
+    }
+    
+    public void insert20pCoin()
+    {
+        balance = balance + 20;
+    }
+    
+    public void insert10pCoin()
+    {
+        balance = balance + 10;
+    }
+    
+    public void insert100Coin()
+    {
+        balance = balance + 100;
+    }
+    
+    public void insert200Coin()
+    {
+        balance = balance + 200;
+    }
+    
+    
+}
