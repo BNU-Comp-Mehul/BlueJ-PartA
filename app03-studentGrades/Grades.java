dc88af2d76023597aee72b97dec93cbd72c57d2b
mehulpatel1 <mehulpatel_@outlook.com>
2020-10-25 11:03:51 +0000

    Completed in class

---
+++ app03-studentGrades/Grades.java
@@ -0,0 +1,10 @@
+/**
+ * Enumeration class Grades - write a description of the enum class here
+ *
+ * @author Mehul Patel
+ * Modified on 23.10.2020
+ */
+public enum Grades
+{
+    X, F, D, C, B, A
+}
