854878a2b0c9fe413f9b3da1cb453d91b57c2eac
mehulpatel1 <mehulpatel_@outlook.com>
2020-10-27 19:31:34 +0000

    Updated print and set mark

--- app03-studentgrades/package.bluej/Course.java
+++ app03-studentgrades/package.bluej/Course.java
@@ -12,15 +12,15 @@ public class Course
 {
     private String codeNo;
     private String title;
-  
+
     private Module module1;
     private Module module2;
     private Module module3;
     private Module module4;
-    
+
     private Grades finalGrade;
     private int finalMark;
-    
+
     /**
      * Constructor for objects of class Course.
      */
@@ -28,13 +28,13 @@ public class Course
     {
         this.codeNo = codeNo;
         this.title = title;
-        
+
         this.finalMark = 0;
         this.finalGrade = null;
-        
+
         createModules();
     }
-    
+
     /**
      * Creates the modules.
      */
@@ -55,49 +55,49 @@ public class Course
         {
             this.module1 = module;
         }
-        
+
         else if (moduleNo == 2)
         {
             this.module2 = module;
         }
-        
+
         else if (moduleNo == 3)
         {
             this.module3 = module;
         }
-        
+
         else if (moduleNo == 4)
         {
             this.module4 = module;
         }
     }
-    
+
     /**
      * Sets the marks for modules.
      */
-    public void setMark(int mark, String codeNo)
+    public void setMark(int mark, int moduleNo)
     {
-        if (module1.getCodeNo() == codeNo)
+        if (moduleNo == 1)
         {
             module1.awardMark(mark);
         }
-        
-        else if (module2.getCodeNo() == codeNo)
+
+        else if (moduleNo == 2)
         {
             module2.awardMark(mark);
         }
-        
-        else if (module3.getCodeNo() == codeNo)
+
+        else if (moduleNo == 3)
         {
             module3.awardMark(mark);
         }
-        
-        else if (module4.getCodeNo() == codeNo)
+
+        else if (moduleNo == 4)
         {
             module4.awardMark(mark);
         }
     }
-    
+
     /**
      * Calculates the final mark for modules.
      */
@@ -105,13 +105,12 @@ public class Course
     {
         if (courseCompleted())
         {
-        int totalMark = module1.getMark() + module2.getMark() + module3.getMark() + module4.getMark();
-        
-        finalMark = totalMark / 4;
-        print();
+            int totalMark = module1.getMark() + module2.getMark() + module3.getMark() + module4.getMark();
+
+            finalMark = totalMark / 4;
         }
     }
-    
+
     /**
      * Checks if course has been completed.
      */
@@ -121,33 +120,41 @@ public class Course
         {
             return true;
         }
-        
+
         else 
         {
             return false;
         }
     }
-    
+
     /**
      * Prints out the details of the course.
      */
     public void print()
     {
         System.out.println("Course " + codeNo + " - " + title);
-        System.out.println("Final Mark = " + finalMark);
+        printModules();
+
+        if (courseCompleted())
+        {
+            System.out.println("Final Mark = " + finalMark);
+            System.out.println("Final Grade = " + finalGrade);
+        }
     }
-    
+
     /**
      * Prints all the module marks.
      */
     public void printModules()
     {
-        if (courseCompleted())
-        {
-            System.out.println("Final Mark = " + finalMark);
-        }
+        // syste.out.printlm("Module List ");
+        module1.print();
+        module2.print();
+        module3.print();
+        module4.print();
+
     }
-    
+
     /**
      * Converts the module marks into grades. 
      */
@@ -157,22 +164,22 @@ public class Course
         {
             finalGrade = Grades.F;
         }
-        
+
         else if ((mark >= 40) && (mark < 50))
         {
             finalGrade = Grades.D;
         }
-        
+
         else if ((mark >= 50) && (mark < 60))
         {
             finalGrade = Grades.C;
         }
-        
+
         else if ((mark >= 60) && (mark < 70))
         {
             finalGrade = Grades.B;
         }
-        
+
         else if ((mark >= 70) && (mark < 100))
         {
             finalGrade = Grades.A;
